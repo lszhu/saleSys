d6f594d27a28fa40bec848b90bea7d06a30710fa
Jason Zhu <jasonzhu.dev@gmail.com>
2015-05-27 17:24:51 +0800

    add header nav-bar

--- client/templates/application/header.js
+++ client/templates/application/header.js
@@ -8,51 +8,54 @@ Template.header.helpers({
     });
 
     return active && 'active';
+  },
+
+  menuInfo: function () {
+    return {
+      message: {
+        name: '消息',
+        operation: [
+          {name: '创建', template: 'createMessage'},
+          {name: '查询', template: 'queryMessage'}
+        ]
+      },
+      base: {
+        name: '索引',
+        operation: [
+          {name: '客户信息', template: 'customerBase'},
+          {name: '员工信息', template: 'employeeBase'},
+          {name: '产品型号信息', template: 'customerBase'}
+        ]
+      },
+      order: {
+        name: '订单',
+        operation: [
+          {name: '添加订单', template: 'createOrder'},
+          {name: '查询与变更', template: 'queryOrder'},
+          {name: '订单处理', template: 'disposeOrder'}
+        ]
+      },
+      report: {
+        name: '报表',
+        operation: [
+          {name: '库存汇总表', template: 'report'},
+          {name: '不良品分析', template: 'report'},
+          {name: '出入库记录', template: 'report'},
+          {name: '订单汇总表', template: 'report'},
+          {name: '资金汇总表', template: 'report'},
+          {name: '资金收支表', template: 'report'},
+          {name: '成本核算表', template: 'report'}
+        ]
+      },
+      admin: {
+        name: '系统管理',
+        operation: [
+          {name: '用户管理', template: 'userAdmin'},
+          {name: '权限管理', template: 'privilegeAdmin'},
+          {name: '消息设置', template: 'messageAdmin'},
+          {name: '销售部管理', template: 'stationCreate'}
+        ]
+      }
+    };
   }
 });
-
-Template.header.data = {
-  message: {
-    name: '消息',
-    operation: [
-      {name: '创建', template: 'createMessage'},
-      {name: '查询', template: 'queryMessage'}
-    ]
-  },
-  base: {
-    operation: [
-      {name: '客户信息', template: 'customerBase'},
-      {name: '员工信息', template: 'employeeBase'},
-      {name: '产品型号信息', template: 'customerBase'}
-    ]
-  },
-  order: {
-    name: '订单',
-    operation: [
-      {name: '添加订单', template: 'createOrder'},
-      {name: '查询与变更', template: 'queryOrder'},
-      {name: '订单处理', template: 'disposeOrder'}
-    ]
-  },
-  report: {
-    name: '报表',
-    operation: [
-      {name: '库存汇总表', template: 'report'},
-      {name: '不良品分析', template: 'report'},
-      {name: '出入库记录', template: 'report'},
-      {name: '订单汇总表', template: 'report'},
-      {name: '资金汇总表', template: 'report'},
-      {name: '资金收支表', template: 'report'},
-      {name: '成本核算表', template: 'report'}
-    ]
-  },
-  admin: {
-    name: '系统管理',
-    operation: [
-      {name: '用户管理', template: 'userAdmin'},
-      {name: '权限管理', template: 'privilegeAdmin'},
-      {name: '消息设置', template: 'messageAdmin'},
-      {name: '销售部管理', template: 'stationCreate'}
-    ]
-  }
-};
---
+++ server/config.js
@@ -0,0 +1,6 @@
+// 系统账号配置
+Accounts.config({
+  // 禁止直接从客户端调用createUser创建登录账号
+  // 同时不再提供Accounts.ui的创建账号的链接
+  forbidClientAccountCreation: true
+});
