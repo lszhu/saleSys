2871ecd2a05098bfdfdb8eec2a8e46531e1b6c05
Jason Zhu <jasonzhu.dev@gmail.com>
2015-05-28 02:31:58 +0800

    new function to add station data to database

--- client/templates/admin/station.js
+++ client/templates/admin/station.js
@@ -3,8 +3,25 @@ Template.station.helpers({
 });
 
 Template.station.events({
-  'click .add-station': function(e) {
+  'click .edit-station': function(e) {
     e.preventDefault();
     $('#add-station').removeClass('hidden');
+  },
+
+  'submit .add-station': function(e) {
+    e.preventDefault();
+
+    var station = {
+      code: $(e.target).find('[name=code]').val(),
+      name: $(e.target).find('[name=name]').val(),
+      manager: $(e.target).find('[name=manager]').val(),
+      address: $(e.target).find('[name=address]').val(),
+      comment: $(e.target).find('[name=comment]').val(),
+      memo: $(e.target).find('[name=memo]').val()
+    };
+    console.log('station: ' + JSON.stringify(station));
+    var overlap = $(e.target).find('[name=overlap]').prop('checked');
+    console.log('overlap is: ' + overlap);
+    Meteor.call('stationInsert', {station: station, overlap: overlap});
   }
-})
+});
