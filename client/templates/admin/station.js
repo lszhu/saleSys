5db2a32317c5ecbc91422fb52b2103065dc3012c
Jason Zhu <jasonzhu.dev@gmail.com>
2015-05-30 12:27:46 +0800

    fix some bugs when edit/update station info

--- client/templates/admin/station.js
+++ client/templates/admin/station.js
@@ -25,11 +25,17 @@ Template.station.events({
 
   'click .edit-station': function (e) {
     e.preventDefault();
+    var target = $('#add-station');
+    // 如果设置了覆盖标识（overlap）则清空，否则只是简单的显示/隐藏切换编辑框
+    if (target.find('[name=overlap]').val()) {
+      target.find('[name=overlap]').val('');
+    } else {
+      target.toggleClass('hidden');
+    }
     // 清空表单中填入的内容
-    clearForm(e.target);
-    // 清空隐藏文本框中保存的数据库条目Id
-    $('#add-station').find('[name=overlap]').val('');
-    $('#add-station').removeClass('hidden');
+    //clearForm(target);
+    // 显示编辑框
+    //target.removeClass('hidden');
   },
 
   'click .update-station': function(e) {
@@ -39,6 +45,7 @@ Template.station.events({
     //console.log('_id: ' + _id);
     // 保存到隐藏的文本框，表示本次操作会强行覆盖对应的数据库条目
     $('#add-station').find('[name=overlap]').val(_id);
+    // 显示编辑框
     $('#add-station').removeClass('hidden');
     fillForm(_id);
   },
@@ -52,6 +59,8 @@ Template.station.events({
     var _id = $(e.currentTarget).attr('href');
     //console.log('_id: ' + _id);
     Meteor.call('stationRemove', _id);
+    // 清空覆盖（overlap）标识，用户点了'变更'后，又马上删除该条目就需要如下处理
+    $('#add-station').find('[name=overlap]').val('');
   },
 
   'submit .add-station': function (e) {
@@ -70,17 +79,21 @@ Template.station.events({
     var overlap = form.find('[name=overlap]').val();
     console.log('overlap is: ' + overlap);
     Meteor.call('stationInsert', {station: station, overlap: overlap});
+    // 最后清除表单的内容
+    clearForm(e.target);
   }
 });
 
 function clearForm(target) {
-  var form = $('target');
+  var form = $(target);
   form.find('[name=code]').val('');
   form.find('[name=name]').val('');
   form.find('[name=manager]').val('');
   form.find('[name=address]').val('');
   form.find('[name=comment]').val('');
   form.find('[name=memo]').val('');
+  // 清空隐藏文本框中保存的数据库条目Id，即清空覆盖标识
+  form.find('[name=overlap]').val('');
 }
 
 function fillForm(_id) {
