8b8285cd04e4f12be674e108a2508f86d087deba
Jason Zhu <jasonzhu.dev@gmail.com>
2015-10-22 11:12:23 +0800

    set correct environment variables for compiled system to run correctly

--- start.bat
+++ start.bat
@@ -2,10 +2,14 @@
 #cd saleSys
 #cd programs/server && npm install
 #
-# 注意：如果package.json中的fiber（由Meteor定制的）包无法编译安装，
-# 则修改对应以来条目，直接装原始的fiber包（需要visual studio）
+# 注意：如果package.json中的fiber编译安装，需要visual studio
 # -----------------------------------------------------------
 
+set initAccount={"username": "admin", "password": "admin"}
+set voucherDir={"storeDir": "/voucher", "uploadDir": "/upload", "tmpDir": "/upload/tmp"}
+set METEOR_SETTINGS={"initAccount": %initAccount%, "voucherDir": %voucherDir%}
+set initAccount=
+set voucherDir=
 set ROOT_URL=http://localhost
 set PORT=3000
 set MONGO_URL=mongodb://salesys:salesys@localhost:27017/salesys
